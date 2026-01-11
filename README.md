163945fb85f52ac033c6a9895ae8a63e3a7cb668
Self-Learner00 <rashminoak21@gmail.com>
2026-01-11 17:13:49 +0530

    Added Python variables example

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # My-first-repository-on-github
 This is my very first repository in github on Python programming language
+<br>
 Author: Rashmin Suyog Oak
---
+++ variables in Python.py
@@ -0,0 +1,6 @@
+''' Variables are memory operators who hold a specific value in a program, either from the beginning or later by the user'''
+a = 5
+b = 10
+''' Here a and b are variables who are assigned with the values 5 and 10 respectively'''
+c = int(input("Enter a number:"))
+'here c is a variable but unlike a and b,its value is not preassigned and has to be assigned by the user'
