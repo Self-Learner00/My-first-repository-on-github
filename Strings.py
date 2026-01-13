b30cd318aca75897a07b9d87ee042c131142c6df
Self-Learner00 <rashminoak21@gmail.com>
2026-01-14 01:12:07 +0530

    Merge branch 'main' of https://github.com/Self-Learner00/My-first-repository-on-github
    merge main branch

---
+++ Strings.py
@@ -0,0 +1,11 @@
+# String in Python is any number,word,letter or special character that is written in enclosed single or double inverted commas
+String = "17"
+string = 17
+# Here the variable String has the value "17" stored in it whereas the variable string has the value 17 in it
+#initially it looks the same but the value "17" is string and the value 17 is int
+print(type(String))
+print(type(string))
+# We will get the output as follows:
+# <class 'str'> for the value of variable String which is "17"
+# <class 'int'> for the value of variable string which is 17
+# String is extremely essential in Python as they lie at the core of Python programming language
